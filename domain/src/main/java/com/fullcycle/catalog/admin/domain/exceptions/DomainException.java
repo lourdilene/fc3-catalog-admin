13ab7abadbadbab7a81f5b81ba7146ba7aabdccf
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-07-26 11:00:53 -0300

    add NoStacktraceExpection

--- domain/src/main/java/com/fullcycle/catalog/admin/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/fullcycle/catalog/admin/domain/exceptions/DomainException.java
@@ -4,16 +4,20 @@ import com.fullcycle.catalog.admin.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException{
+public class DomainException extends NoStacktraceException{
     private final List<Error> errors;
 
-    public DomainException(final List<Error> anErrors) {
-        super("", null ,true, false);
+    public DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
+    public static DomainException with(final Error anErrors){
+        return new DomainException(anErrors.message(), List.of(anErrors));
+    }
+
     public static DomainException with(final List<Error>anErrors){
-        return new DomainException(anErrors);
+        return new DomainException("",anErrors);
     }
 
     public List<Error> getErrors() {
